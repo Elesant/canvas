2d95491178aba4d8268fa9c6c9a76f6e2e772b7d
Sean Li <mail@shang.li>
2015-08-18 11:39:17 -0700

    Website Pages

--- tools/deploy.js
+++ tools/deploy.js
@@ -3,9 +3,16 @@
  * https://github.com/koistya/react-static-boilerplate
  * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
  */
+import push from 'git-push';
+
+const remote = {
+  name: 'origin',
+  url: 'https://github.com/Elesant/canvas',
+  branch: 'gh-pages'
+};
 
 export default async () => {
-  console.log('deploy');
   await require('./build')();
-  console.log('TODO: deploy to GitHub pages');
+  await new Promise(resolve => push('./build', remote, resolve));
 };
+
